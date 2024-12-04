76d41c1c99222649f92a4507ce1aba1475e13129
Lucas <42320754+kuniix@users.noreply.github.com>
2024-12-04 08:59:17 -0300

    Ordenar consulta no banco de dados

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
+    search = request.GET.get('search')
+    
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+    else:
+        cars = Car.objects.all()
    
     return render(request,
                   'cars.html',
-                  {'cars': {'model': 'Skyline R34'}})
+                  {'cars': cars})
 
 def cars_value(request):
     return HttpResponse("Os carros mais valiosos.")
